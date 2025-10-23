18e9afbb5e23539e20b5f907e083bf69de3f41dc
amirkhanhard <ak9789858@gmail.com>
2025-10-24 00:55:41 +0530

    added tsc and ts-node

---
+++ tsc/src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum = (a, b) => {
+    console.log(a, b);
+    return a + b;
+};
+const su = sum(1, 2);
---
+++ tsc/src/main.ts
@@ -0,0 +1,5 @@
+const sum = (a:any,b:any) => {
+    console.log(a,b);
+    return a+b;
+}
+const su = sum(1,2);
