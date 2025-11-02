dbb18b8a02bb5aa63fbb0a25b207377d1ceb0b1f
amirkhanhard <ak9789858@gmail.com>
2025-11-03 03:44:13 +0530

    added guide

--- Inference.ts
+++ Inference.ts
@@ -5,17 +5,21 @@
 let a = "asdf";
 let b = 12345;
 let c = true;
+let d: any = "asdf";
+let e: RegExp = /^[a-z]$/
 
 console.log(a,b,c);
 console.log( typeof a);//string
 console.log( typeof b);//number
 console.log( typeof c);//bolean
+console.log( typeof d);//string
 
 
 // a = 5;  // Type 'number' is not assignable to type 'string'.
 // b = "asdf" // Type 'string' is not assignable to type 'number'.
 // c = 123 // Type 'number' is not assignable to type 'boolean'.
 
-let d;
-console.log(typeof d);//undefined
-d = 55;
+let f;
+console.log(typeof f);// undefined
+f = 55;
+
